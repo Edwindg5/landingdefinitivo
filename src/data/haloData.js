e7368d853496b754b1e16e4629d90fac2f8b3454
Edwindg5 <152365021+Edwindg5@users.noreply.github.com>
2024-07-31 12:24:47 -0600

    fix images

--- src/data/haloData.js
+++ src/data/haloData.js
@@ -1,19 +1,19 @@
 const haloData = {
     backgroundImage: { src: '/background.jpeg', alt: 'Background' },
     products: [
-      { src: '/halo1.jpeg', text: 'Halo 1: Combat Evolved' },
-      { src: '/halo2.jpeg', text: 'Halo 2: Anniversary' },
-      { src: '/halo3.jpeg', text: 'Halo 3: ODST' },
+      { src: 'halo1.jpeg', text: 'Halo 1: Combat Evolved' },
+      { src: 'halo2.jpeg', text: 'Halo 2: Anniversary' },
+      { src: 'halo3.jpeg', text: 'Halo 3: ODST' },
     ],
     posters: [
-      { src: '/halo-poster1.jpeg', alt: 'Halo Poster 1' },
-      { src: '/halo-poster2.jpeg', alt: 'Halo Poster 2' },
-      { src: '/halo-poster3.jpeg', alt: 'Halo Poster 3' },
+      { src: 'halo-poster1.jpeg', alt: 'Halo Poster 1' },
+      { src: 'halo-poster2.jpeg', alt: 'Halo Poster 2' },
+      { src: 'halo-poster3.jpeg', alt: 'Halo Poster 3' },
     ],
-    logo: { src: '/halo-logo.jpeg', alt: 'Halo Logo' },
+    logo: { src: 'halo-logo.jpeg', alt: 'Halo Logo' },
     explore: [
-      { src: '/halo-banner1.jpeg', alt: 'Halo Banner 1' },
-      { src: '/halo-banner2.jpeg', alt: 'Halo Banner 2' },
+      { src: 'halo-banner1.jpeg', alt: 'Halo Banner 1' },
+      { src: 'halo-banner2.jpeg', alt: 'Halo Banner 2' },
     ],
     additionalInfo: [
       { title: 'Sobre el Desarrollo', text: 'Halo es desarrollado por 343 Industries, un estudio de desarrollo de videojuegos propiedad de Microsoft.' },
