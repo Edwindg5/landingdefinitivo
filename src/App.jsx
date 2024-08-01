b24242c01755a3b3d7535ac8ee0f26b298222438
Edwindg5 <152365021+Edwindg5@users.noreply.github.com>
2024-07-31 18:33:28 -0600

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,7 @@ import './index.css';
 import haloData from './data/haloData';
 
 function App() {
-  const appStyle = {
-    backgroundImage: `url(${haloData.backgroundImage.src})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center center',
-    backgroundSize: 'cover',
-    backgroundAttachment: 'fixed',
-    minHeight: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-  };
+
 
   return (
     <div className="app" style={appStyle}>
